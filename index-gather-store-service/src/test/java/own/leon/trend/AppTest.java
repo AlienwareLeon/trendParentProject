a44e59faf581995dced93ab82debe4ccd4d4e82c
Alienwareleon <alienwareleon@gmail.com>
2019-08-12 20:37:18 +0800

    trend_springcloud

---
+++ index-gather-store-service/src/test/java/own/leon/trend/AppTest.java
@@ -0,0 +1,20 @@
+package own.leon.trend;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
